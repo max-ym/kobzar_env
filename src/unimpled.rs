80a61472600be5cbc5edddd3838a6758ee5a17dc
Max Naumchyk <max.naumch@gmail.com>
2020-05-14 02:26:13 +0300

    Fixed compilation errors due to missing implementations

--- src/unimpled.rs
+++ src/unimpled.rs
@@ -1,8 +1,9 @@
 #![allow(dead_code)]
 use crate::{KobzarEnv, PrivateKobzarEnv, Uid};
 use crate::path::{Network, FindInstanceRequest, InstanceId};
-use crate::harc::Darc;
+use crate::harc::{Darc, Snapshot};
 use smallvec::SmallVec;
+use crate::thread::{OwnedThread, ThreadBuildError, ThreadBuilder};
 
 pub struct UnimplementedEnv;
 pub struct UnimplementedNetwork;
@@ -15,6 +16,10 @@ impl KobzarEnv for UnimplementedEnv {
     }
 
     fn network_mut(&mut self) -> &mut Self::Network {
+        unimplemented!()
+    }
+
+    fn download_new_snapshot<T: Clone>(&self, _: &Snapshot<T>) -> Snapshot<T> {
         unimplemented!()
     }
 }
@@ -32,6 +37,10 @@ pub fn kobzar_env() -> &'static mut UnimplementedEnv {
 impl Network for UnimplementedNetwork {
     fn find_package_instances(&self, _: &FindInstanceRequest)
                               -> SmallVec<[Darc<InstanceId>; 16]> {
+        unimplemented!()
+    }
+
+    fn create_thread(&mut self, _: &ThreadBuilder) -> Result<OwnedThread, ThreadBuildError> {
         unimplemented!()
     }
 }
