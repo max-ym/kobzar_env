7fad497050d5385b610e93a5523393421c9056c7
Max Naumchyk <max.naumch@gmail.com>
2020-05-14 20:05:45 +0300

    Further extended msg.rs

--- src/msg.rs
+++ src/msg.rs
@@ -6,6 +6,7 @@ use alloc::rc::Rc;
 use crate::rsc::{Handle, Constant};
 use core::ops::Deref;
 use crate::Uid;
+use core::marker::PhantomData;
 
 pub enum SendError {
     /// Receiver does not implement interface for which this message was formed.
@@ -67,4 +68,67 @@ impl<T> SharedMessage<T> where T: Send + Copy {
     pub fn share_with(&self, with: &Thread) -> Result<(), ShareError> {
         unimplemented!()
     }
+}
+
+/// Information about received message and the message itself.
+pub struct Envelop {
+    /// Interface for this message.
+    interface: Rc<Interface>,
+
+    /// Sender (source) of the message.
+    source: Rc<Thread>,
+}
+
+pub struct Received<T: Copy + Send> {
+    source: Rc<Thread>,
+    msg: Message<T>,
+}
+
+impl Envelop {
+    /// Accept the envelop and read the message as given type.
+    ///
+    /// # Safety
+    /// Receiver must guarantee the chosen type corresponds to the interface stated
+    /// in the envelop. Also, there are no guarantee that sender used the correct type
+    /// and maintained type consistency. For complex types it is best to have trusted
+    /// message sources.
+    pub unsafe fn accept<T: Send + Copy>(self) -> Received<T> {
+        unimplemented!()
+    }
+}
+
+/// Channel makes simpler to maintain intensive communication between threads.
+pub struct Pipe<T: Send + Copy> {
+    destination: Rc<Thread>,
+    interface: Rc<Interface>,
+    _msg_type: PhantomData<Message<T>>,
+}
+
+impl<T: Send + Copy> Pipe<T> {
+    pub fn receive(&self) -> Option<Message<T>> {
+        unimplemented!()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        unimplemented!()
+    }
+}
+
+/// Receive pending envelops. The envelops are received in FIFO order thus the oldest message
+/// gets read first.
+///
+/// None is returned if mailbox is empty.
+pub fn receive() -> Option<Envelop> {
+    unimplemented!()
+}
+
+/// Whether the mailbox is empty.
+pub fn is_empty() -> bool {
+    unimplemented!()
+}
+
+// TODO remove functional style in favor of OOP.
+/// Received the message from given thread if any.
+pub fn receive_from(thread: &Thread) -> Option<Envelop> {
+    unimplemented!()
 }
