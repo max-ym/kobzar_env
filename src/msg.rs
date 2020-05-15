ae9cfacf035bb77d23a4963bc47610291d1d0583
Max Naumchyk <max.naumch@gmail.com>
2020-05-16 01:48:29 +0300

    Started messaging revision

--- src/msg.rs
+++ src/msg.rs
@@ -1,134 +1,57 @@
 //! Messaging
 
-use crate::thread::Thread;
-use crate::path::{Interface};
-use alloc::rc::Rc;
-use crate::rsc::{Handle, Constant};
-use core::ops::Deref;
-use crate::Uid;
 use core::marker::PhantomData;
+use alloc::rc::Rc;
+use crate::thread::Thread;
+use crate::path::Interface;
+
+pub struct InputPipe<I: Input> {
+    src: Rc<Thread>,
+    interface: Rc<Interface>,
+    _input: PhantomData<I>,
+}
+
+pub struct OutputPipe<O: Output> {
+    dest: Rc<Thread>,
+    interface: Rc<Interface>,
+    _output: PhantomData<O>,
+}
+
+/// Message that can be sent through the pipe.
+pub trait Input {
+    /// Construct object from bytes received from the Receiver.
+    fn from_msg_bytes(b: &[u8]) -> Self;
+}
+
+pub trait Output {
+    /// Bytes that will be sent by the pipe.
+    fn as_msg_bytes(&self) -> &[u8];
+}
 
 pub enum SendError {
-    /// Receiver does not implement interface for which this message was formed.
-    InterfaceNotImplemented,
+    /// Received has died.
+    Died,
+
+    /// Connection with this thread was lost.
+    ConnectionLost,
 }
 
-pub enum ShareError {
-    /// Receiver does not implement interface for which this message was formed.
-    InterfaceNotImplemented,
-}
-
-#[derive(Clone)]
-pub struct Message<T: Send + Copy> {
-    msg: T,
-    interface: Rc<Interface>,
-}
-
-impl<T> Message<T> where T: Send + Copy {
-    /// Copy message into destination thread's memory.
-    pub fn copy_to(&self, to: &Thread) -> Result<(), SendError> {
-        unimplemented!()
-    }
-}
-
-impl Message<()> {
-    /// Create new signal. Signal is a message with no body. It just signals the receiver
-    /// by the interface.
-    pub fn new_signal(interface: Rc<Interface>) -> Self {
-        Message {
-            msg: (),
-            interface,
-        }
-    }
-}
-
-#[derive(Clone)]
-pub struct SharedMessage<T: Send + Copy> {
-    msg: Message<T>,
-    uid: Uid,
-}
-
-impl<T> Handle for SharedMessage<T> where T: Send + Copy {
-    fn uid(&self) -> Uid {
-        self.uid
-    }
-}
-
-impl<T> Constant for SharedMessage<T> where T: Send + Copy {}
-
-impl<T> Deref for SharedMessage<T> where T: Send + Copy {
-    type Target = Message<T>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.msg
-    }
-}
-
-impl<T> SharedMessage<T> where T: Send + Copy {
-    pub fn share_with(&self, with: &Thread) -> Result<(), ShareError> {
-        unimplemented!()
-    }
-}
-
-/// Information about received message and the message itself.
-pub struct Envelop {
-    /// Interface for this message.
-    interface: Rc<Interface>,
-
-    /// Sender (source) of the message.
-    source: Rc<Thread>,
-}
-
-pub struct Received<T: Copy + Send> {
-    source: Rc<Thread>,
-    msg: Message<T>,
-}
-
-impl Envelop {
-    /// Accept the envelop and read the message as given type.
+impl<O: Output> OutputPipe<O> {
+    /// Send message into mailbox. Note that this does not guarantee that the message
+    /// will be received. Receiver may also discard the message or cease without reading.
+    /// This method does not block and message will be buffered in current Pipe output buffer.
+    /// If pipe has no buffer that this is the same as [`rendezvous`] method.
     ///
-    /// # Safety
-    /// Receiver must guarantee the chosen type corresponds to the interface stated
-    /// in the envelop. Also, there are no guarantee that sender used the correct type
-    /// and maintained type consistency. For complex types it is best to have trusted
-    /// message sources.
-    pub unsafe fn accept<T: Send + Copy>(self) -> Received<T> {
-        unimplemented!()
-    }
-}
-
-/// Channel makes simpler to maintain intensive communication between threads.
-pub struct Pipe<T: Send + Copy> {
-    destination: Rc<Thread>,
-    interface: Rc<Interface>,
-    _msg_type: PhantomData<Message<T>>,
-}
-
-impl<T: Send + Copy> Pipe<T> {
-    pub fn receive(&self) -> Option<Message<T>> {
+    /// Error will occur if thread is no longer alive or connection is lost.
+    pub fn send(&self, msg: O) -> Result<(), SendError> {
         unimplemented!()
     }
 
-    pub fn is_empty(&self) -> bool {
+    /// Send the message by making rendezvous with the receiver. This makes a guarantee that
+    /// receiver actually acquired the message but it still does not guarantee that the
+    /// message was read as receiver can discard it. This method will execute
+    /// as soon as all previous messages will get received.
+    pub fn rendezvous(&self, msg: O) -> Result<(), SendError> {
         unimplemented!()
     }
-}
-
-/// Receive pending envelops. The envelops are received in FIFO order thus the oldest message
-/// gets read first.
-///
-/// None is returned if mailbox is empty.
-pub fn receive() -> Option<Envelop> {
-    unimplemented!()
-}
-
-/// Whether the mailbox is empty.
-pub fn is_empty() -> bool {
-    unimplemented!()
-}
-
-// TODO remove functional style in favor of OOP.
-/// Received the message from given thread if any.
-pub fn receive_from(thread: &Thread) -> Option<Envelop> {
-    unimplemented!()
 }
